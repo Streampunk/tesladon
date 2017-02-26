828b95d04a464f5885e4d6b1ce0146c84707e242
Richard Cartwright <spark@streampunk.media>
2017-02-26 15:00:33 +0000

    Added tests for roundtrip of TS to PTP timestamps.

--- test/UtilSpec.js
+++ test/UtilSpec.js
@@ -15,6 +15,8 @@
 
 var readTimeStamp = require('../src/util.js').readTimeStamp;
 var writeTimeStamp = require('../src/util.js').writeTimeStamp;
+var tsTimeToPTPTime = require('../src/util.js').tsTimeToPTPTime;
+var ptpTimeToTsTime = require('../src/util.js').ptpTimeToTsTime;
 var test = require('tape');
 
 function getRandomInt(min, max) {
@@ -91,6 +93,31 @@ test('Is OK for 1000 random values', t => {
     writeTimeStamp(n, 0, b, 0);
     t.equal(readTimeStamp(b, 0), n, `roundtrips OK for random value ${n}.`);
     t.ok(checkOnes(b), 'has reserved values set to 1.');
+  };
+  t.end();
+});
+
+var roundTime = x => ptpTimeToTsTime(tsTimeToPTPTime(x));
+const tsDay = Math.pow(2, 33);
+
+test('Check TS to PTP timestamp edge values', t => {
+  t.equal(roundTime(0), 0, "zero roundtrips OK.");
+  t.equal(roundTime(tsDay), 0, "2**33 wraps around to 0.");
+  t.equal(roundTime(tsDay - 1), tsDay - 1, "maximum value is preserved.");
+  t.equal(roundTime(tsDay + 1), 1, "maximum value plus one wraps around.");
+  t.equal(roundTime(tsDay * 2), 0, "double wrap around.");
+  t.end();
+});
+
+test('Timestamps roundtrip at 1000 random values', t => {
+  for ( var x = 0 ; x < 1000 ; x++ ) {
+    var ts = Math.floor((Math.random() * tsDay));
+    var ptp = tsTimeToPTPTime(ts);
+    t.ok(Array.isArray(ptp) && ptp.length === 2, 'PTP is an array length 2.');
+    t.ok(typeof ptp[0] === 'number' && typeof ptp[1] === 'number', 'elements are numbers.');
+    t.ok(ptp[0] > (Date.now() / 1000 - 100000), 'seconds part is recent.');
+    t.ok(ptp[1] < 1000000000, 'part seconds is less than a second.');
+    t.equal(ptpTimeToTsTime(ptp), ts, `roundtrip of value ${ts} OK.`);
   };
   t.end();
 });
